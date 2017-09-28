f8f4c41369d5e984dbf2663cc8f3b1c6ebf84af1
Sayo Ogunlegan <ogunlegano@gmail.com>
2017-09-27 17:54:33 -0700

    initial

---
+++ lib/soup.ex
@@ -0,0 +1,18 @@
+defmodule Soup do
+  @moduledoc """
+  Documentation for Soup.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Soup.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
